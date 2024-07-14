ce008e3fb1501fc2a7f6ebae2bbf776077f1dbff
Daniel Shanahan <daniel.patrick.shanahan@gmail.com>
2024-07-14 13:48:14 -0400

    Added post model

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post, User
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("poster", "body", "timestamp")
+
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Post, PostAdmin)
--- network/models.py
+++ network/models.py
@@ -4,3 +4,20 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    poster = models.ForeignKey("User", on_delete=models.CASCADE, related_name="posts")
+    body = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.poster.username}: {self.body}"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "poster": self.poster.username,
+            "body": self.body,
+            "timestamp": self.timestamp.strf("%b %d %Y, %I:%M %p"),
+        }
