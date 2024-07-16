01d4570827cb8497a3fcadf05c0fddba023c5598
Daniel Shanahan <daniel.patrick.shanahan@gmail.com>
2024-07-16 15:40:09 -0400

    Added a basic test for model relationship

--- network/tests.py
+++ network/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 
-# Create your tests here.
+from .models import User, Post
+
+
+class NetworkTestCase(TestCase):
+
+    def setUp(self):
+        # Create users.
+        u1 = User.objects.create(username="Harry", password="Potter")
+        u2 = User.objects.create(username="Ron", password="Weasley")
+
+        # Create posts.
+        p1 = Post.objects.create(poster=u1, body="Malfoy is a jerk")
+        p2 = Post.objects.create(poster=u2, body="Gryffindor rules!")
+        p3 = Post.objects.create(poster=u1, body="Why is Snape such a jerk?")
+
+    def test_posts_count(self):
+        u = User.objects.get(username="Harry")
+        self.assertEqual(u.posts.count(), 2)
