6070dabf7ed0aabfdc3f73cd2fc475ef44ed324a
Daniel Shanahan <daniel.patrick.shanahan@gmail.com>
2024-07-18 13:13:42 -0400

    Following/followers tests

--- network/tests.py
+++ network/tests.py
@@ -15,6 +15,23 @@ class NetworkTestCase(TestCase):
         p2 = Post.objects.create(poster=u2, body="Gryffindor rules!")
         p3 = Post.objects.create(poster=u1, body="Why is Snape such a jerk?")
 
+        # Follow users.
+        u2.following.add(u1)
+
     def test_posts_count(self):
         u = User.objects.get(username="Harry")
         self.assertEqual(u.posts.count(), 2)
+
+    def test_following_count(self):
+        u1 = User.objects.get(username="Harry")
+        u2 = User.objects.get(username="Ron")
+
+        self.assertEqual(u1.following.count(), 0)
+        self.assertEqual(u2.following.count(), 1)
+
+    def test_followers_count(self):
+        u1 = User.objects.get(username="Harry")
+        u2 = User.objects.get(username="Ron")
+
+        self.assertEqual(u1.followers.count(), 1)
+        self.assertEqual(u2.followers.count(), 0)
