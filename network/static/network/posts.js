42d8771daee5169e83d9096db8c968b2e677725f
Daniel Shanahan <daniel.patrick.shanahan@gmail.com>
2024-07-21 17:31:10 -0400

    Added javascript

---
+++ network/static/network/posts.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Use like and unlike buttons on posts
+  document.querySelectorAll("#like").forEach((likeButton) => {
+    likeButton.addEventListener("click", function () {});
+  });
+});
